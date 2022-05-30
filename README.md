22b6ac922c6171313ada990472d8d4979a7d09f1
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-05-30 13:29:22 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# NEXTJS - Ecommerce
+> Build a E-commerce Full Stack with Nextjs + Mongodb + React Hook + Bootstrap 4
+> Instructions on how to use nextjs in conjunction with mongodb to build a basic e-commerce website.
+
+## Demo: https://samaka-st-ore.vercel.app/?search=all
+
+## Install dependencies 
+### `npm install`
+
+BASE_URL="http://localhost:3000"
+MONGODB_URL="mongodb+srv://.....................net/SamaKaSTore?retryWrites=true&w=majority"
+ACCESS_TOKEN_SECRET="TRhG>R'dss[q$Ut6^"
+REFRESH_TOKEN_SECRET="?(emx3asaR*nB9>&>XX"
+PAYPAL_CLIENT_ID="AUrwURNjasadseUb9N-gYKaesciAf1IolsL83ehpIQn3_eBUo9yUvrIR3NP_KG_hMbqFORzCNvHjPaD2Z4BfS"
+CLOUD_UPDATE_PRESET="SamaKaSTore Presist"
+CLOUD_NAME="dbolkzi3812"
+CLOUD_API="cloudinary://>>>>>>>>>>>>>>>>>>>>>jENWtW5NFDJVwADzE@dbolkzi38"
+
+
+
+
+## Connect to your mongodb and add info in next.config.js
+
+## Run the project http://localhost:3000
+### `npm run dev`
+
+
+### TODO 
+1. setup i18n and rtl support
+2. add dark theme
+
+
---
+++ pages/index.js
@@ -0,0 +1,97 @@
+import Head from "next/head";
+
+const Home = (props) => {
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <body>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <a className='navbar-brand' href='#'>
+            Navbar
+          </a>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav mr-auto'>
+              <li className='nav-item active'>
+                <a className='nav-link' href='#'>
+                  Home <span className='sr-only'>(current)</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='#'>
+                  Link
+                </a>
+              </li>
+              <li className='nav-item dropdown'>
+                <a
+                  className='nav-link dropdown-toggle'
+                  href='#'
+                  id='navbarDropdown'
+                  role='button'
+                  data-toggle='dropdown'
+                  aria-haspopup='true'
+                  aria-expanded='false'
+                >
+                  Dropdown
+                </a>
+                <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <a className='dropdown-item' href='#'>
+                    Action
+                  </a>
+                  <a className='dropdown-item' href='#'>
+                    Another action
+                  </a>
+                  <div className='dropdown-divider'></div>
+                  <a className='dropdown-item' href='#'>
+                    Something else here
+                  </a>
+                </div>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link disabled' href='#'>
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className='form-inline my-2 my-lg-0'>
+              <input
+                className='form-control mr-sm-2'
+                type='search'
+                placeholder='Search'
+                aria-label='Search'
+              />
+              <button
+                className='btn btn-outline-success my-2 my-sm-0'
+                type='submit'
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </nav>
+      </body>
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  // server side rendering
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
+export default Home;
